ffd66ab4aededbb130d4143e35ba8c3521066200
Fabian Cook <hello@fabiancook.dev>
2022-06-09 23:12:55 +1200

    Sync next

--- src/tests/sync.ts
+++ src/tests/sync.ts
@@ -1,57 +1,81 @@
 
-import {aSyncThing} from "../the-sync-thing";
+import {aSyncThing, TheSyncThing} from "../the-sync-thing";
 import {isIteratorYieldResult} from "../is";
 
-const thing = aSyncThing([1, 2, 3]);
+async function withThing(thing: TheSyncThing) {
+    async function run() {
 
-async function run() {
+        const one = await thing;
+        const two = await thing;
+        const three = await thing;
 
-    const one = await thing;
-    const two = await thing;
-    const three = await thing;
+        const done = await thing;
 
-    const done = await thing;
-
-    console.log({ one, two, three, done });
-}
-
-await run();
-await run();
-
-async function aWhile() {
-    let next;
-    while (next = await thing) {
-        console.log({ next });
+        console.log({ one, two, three, done });
     }
-}
 
-await aWhile();
-await aWhile();
-async function aWhileNext() {
-    let next, result;
-    while ((result = thing.next()) && isIteratorYieldResult(result) && (next = result.value)) {
-        console.log({ next });
+    await run();
+    await run();
+
+    async function aWhile() {
+        let next;
+        while (next = await thing) {
+            console.log({ next });
+        }
     }
-}
 
-await aWhileNext();
-await aWhileNext();
-
-console.log([ ...thing ]);
-
-async function forAwait() {
-    for await (const next of thing) {
-        console.log({ next });
+    await aWhile();
+    await aWhile();
+    async function aWhileNext() {
+        let next, result;
+        while ((result = thing.next()) && isIteratorYieldResult(result) && (next = result.value)) {
+            console.log({ next });
+        }
     }
-}
 
-async function forSync() {
-    for (const next of thing) {
-        console.log({ next });
+    await aWhileNext();
+    await aWhileNext();
+
+    console.log([ ...thing ]);
+
+    async function forAwait() {
+        for await (const next of thing) {
+            console.log({ next });
+        }
     }
+
+    async function forSync() {
+        for (const next of thing) {
+            console.log({ next });
+        }
+    }
+
+    await forAwait();
+    await forSync();
+    await forAwait();
+    await forSync();
 }
 
-await forAwait();
-await forSync();
-await forAwait();
-await forSync();
+await withThing(aSyncThing([1, 2, 3]));
+const source: Iterator<number> & { value: number, max: number } = {
+    value: 0,
+    max: 10,
+    next(): IteratorResult<number> {
+        const value = source.value;
+        if (value >= source.max) {
+            source.value = 0;
+            return { value: undefined, done: true }
+        } else {
+            source.value += 1;
+            return { value, done: false };
+        }
+    },
+    return() {
+        return { done: true, value: undefined };
+    },
+    throw() {
+        return { done: true, value: undefined }
+    }
+};
+
+await withThing(aSyncThing(source));
