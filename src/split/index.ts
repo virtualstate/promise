3d7d898a6896434a5dbcd3b2112cd809d6815ae3
Fabian Cook <hello@fabiancook.dev>
2022-06-24 23:36:11 +1200

    split with fn filter

--- src/split/index.ts
+++ src/split/index.ts
@@ -1,10 +1,25 @@
 import {anAsyncThing, TheAsyncThing} from "../the-thing";
-import {Push} from "../push";
+import {Push, PushOptions} from "../push";
 import {ok} from "../like";
 
-export function split<T>(input: AsyncIterable<T[]>): Iterable<TheAsyncThing<T>> {
+export interface SplitOptions extends PushOptions {
+
+}
+
+export interface FilterFn<T> {
+    (value: T): boolean
+}
+
+export interface Split<T> extends Iterable<TheAsyncThing<T>>, AsyncIterable<T[]> {
+    filter(value: FilterFn<T>): Split<T>
+}
+
+export function split<T>(input: AsyncIterable<T[]>, options?: SplitOptions): Split<T> {
     const targets: Push<T>[] = [],
-        outputs: TheAsyncThing<T>[] = [];
+        filter = new Map<FilterFn<T>, Push<T[]>>(),
+        splits = new Map<FilterFn<T>, Split<T>>();
+
+    let mainTarget: Push<T[]> | undefined = undefined;
 
     let done = false,
         started = false;
@@ -23,46 +38,80 @@ export function split<T>(input: AsyncIterable<T[]>): Iterable<TheAsyncThing<T>> 
             ok(!started);
             started = true;
             for await (const snapshot of input) {
+                mainTarget?.push(snapshot)
                 for (const [index, value] of Object.entries(snapshot)) {
                     targets[+index]?.push(value);
+
+                }
+                for (const [fn, target] of filter.entries()) {
+                    const filtered = snapshot.filter(fn);
+                    if (!filtered.length) continue;
+                    target.push(filtered);
                 }
             }
+            mainTarget?.close();
             for (const target of targets) {
                 target.close();
             }
+            for (const target of filter.values()) {
+                target.close();
+            }
         } catch (error) {
+            mainTarget?.throw(error);
             for (const target of targets) {
+                target.throw(error);
+            }
+            for (const target of filter.values()) {
                 target.throw(error);
             }
         }
         done = true;
     }
 
-    function getTarget(index: number) {
-        const existing = outputs[index]
-        if (existing) {
-            return existing;
-        }
-        const target = new Push<T>();
-        targets[index] = target;
-        const output = anAsyncThing({
+    function getOutput<Z>(target: Push<Z>): TheAsyncThing<Z> {
+        return anAsyncThing({
             async *[Symbol.asyncIterator]() {
                 const promise = start();
                 yield * target;
                 await promise;
             }
         });
-        outputs[index] = output;
-        return output;
+    }
+
+    function getTarget(index: number) {
+        const existing = targets[index]
+        if (existing) {
+            return getOutput(existing);
+        }
+        const target = new Push<T>(options);
+        targets[index] = target;
+        return getOutput(target);
     }
 
     return {
+        async *[Symbol.asyncIterator]() {
+          mainTarget = mainTarget ?? new Push(options);
+          const promise = start();
+          yield * mainTarget;
+          await promise;
+        },
         [Symbol.iterator]() {
             function * withIndex(index: number): Iterable<TheAsyncThing<T>> {
                 yield getTarget(index);
                 yield * withIndex(index + 1);
             }
             return withIndex(0)[Symbol.iterator]();
+        },
+        filter(fn) {
+            const existing = splits.get(fn);
+            if (existing) {
+                return existing;
+            }
+            const target = new Push<T[]>();
+            filter.set(fn, target);
+            const output = split(getOutput(target));
+            splits.set(fn, output);
+            return output;
         }
     }
 }
--- src/tests/split.ts
+++ src/tests/split.ts
@@ -1,5 +1,6 @@
 import { split } from "../split";
 import {ok} from "../like";
+import {anAsyncThing} from "../the-thing";
 
 {
     const [a, b, c] = split({
@@ -33,4 +34,98 @@ import {ok} from "../like";
 
     console.log({ total });
     ok(total === 6);
+}
+
+{
+    const read = split({
+        async* [Symbol.asyncIterator]() {
+            yield [1, 2, 3];
+            yield [4, 5, 6];
+        }
+    });
+
+    const five = await anAsyncThing(read.filter(value => value === 5));
+    console.log({ five });
+    ok(Array.isArray(five));
+    ok(five[0] === 5);
+
+}
+{
+    const read = split({
+        async* [Symbol.asyncIterator]() {
+            yield [1, 2, 3];
+            yield [4, 5, 6];
+        }
+    });
+
+    const even = await anAsyncThing(read.filter(value => value % 2 === 0));
+    console.log({ even });
+    ok(Array.isArray(even));
+    ok(!even.includes(2)); // Only includes the final snapshot
+    ok(even.includes(4));
+    ok(even.includes(6));
+
+    ok(!even.includes(1));
+    ok(!even.includes(3));
+    ok(!even.includes(5));
+
+}
+
+{
+    const read = split({
+        async* [Symbol.asyncIterator]() {
+            yield [1, 2, 3];
+            yield [4, 5, 6];
+        }
+    });
+
+    for await (const even of read.filter(value => value % 2 === 0)) {
+        console.log({ even });
+        ok(Array.isArray(even));
+        ok(
+            even.includes(2) || (even.includes(4) && even.includes(6))
+        );
+        ok(!even.includes(1));
+        ok(!even.includes(3));
+        ok(!even.includes(5));
+    }
+
+}
+
+{
+    const read = split({
+        async* [Symbol.asyncIterator]() {
+            yield [1, 2, 3];
+            yield [4, 5, 6];
+        }
+    });
+
+    const [first] = read.filter(value => value % 2 === 0);
+
+    for await (const even of first) {
+        console.log({ even });
+        ok(typeof even === "number");
+        ok(even !== 1);
+        ok(even !== 3);
+        ok(even !== 5);
+        ok(even !== 6);
+        ok(
+            even === 2 || even === 4
+        );
+    }
+
+}
+
+{
+    const read = split({
+        async* [Symbol.asyncIterator]() {
+            yield [1, 2, 3];
+            yield [4, 5, 6];
+        }
+    });
+
+    const [first,last] = read.filter(value => value % 2 === 0);
+
+    ok(await first === 4);
+    ok(await last === 6);
 }
