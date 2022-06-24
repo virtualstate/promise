56e0f9b0815164d189ce7804aa6fff4684649abc
Fabian Cook <hello@fabiancook.dev>
2022-06-25 00:14:49 +1200

    add union as an exported function

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,15 @@ import * as Promise from "./promise";
 
 export { Promise };
 
+
 export * from "./promise";
 export * from "./push";
 export * from "./defer";
 export * from "./split";
+
+// Core dependency
+export {
+    union,
+    UnionInput,
+    UnionOptions
+} from "@virtualstate/union";
