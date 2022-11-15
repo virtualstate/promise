44982dc2b71c657d0992252899cf510c738ed23f
Fabian Cook <hello@fabiancook.dev>
2022-11-15 23:30:29 +1300

    add another line example

--- src/tests/line.ts
+++ src/tests/line.ts
@@ -58,3 +58,41 @@ import {ok} from "../like";
     ok((await iterator.previous()).value === 2);
     ok((await iterator.next()).value === 3);
 }
+
+{
+
+    const line = new Line();
+
+    line.push(1);
+    line.push(2);
+    line.push(3);
+
+    const iterator = line[Symbol.asyncIterator]();
+
+    ok((await iterator.next()).value === 1);
+    ok((await iterator.next()).value === 2);
+    ok((await iterator.next()).value === 3);
+
+    let promise = iterator.next();
+
+    let expected = 4 + Math.random();
+    line.push(expected);
+
+    ok((await promise).value === expected);
+
+    ok((await iterator.previous()).value === 3);
+    ok((await iterator.previous()).value === 2);
+    ok((await iterator.previous()).value === 1);
+    ok((await iterator.previous()).done);
+
+    line.push(expected + 1);
+    line.close();
+
+    ok((await iterator.next()).value === 1);
+    ok((await iterator.next()).value === 2);
+    ok((await iterator.next()).value === 3);
+    ok((await iterator.next()).value === expected);
+    ok((await iterator.next()).value === expected + 1);
+    ok((await iterator.next()).done);
+
+}
